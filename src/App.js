ae99ca39ebf4a8ebc5984aa8228c3e99a9f0a221
jeff-0821 <jeff.0821@kakaocorp.com>
2021-09-18 16:42:08 +0900

    get movie data and add style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css'
 
 class App extends React.Component {
   state = {
@@ -12,17 +15,42 @@ class App extends React.Component {
     }))
   }
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: {
+          movies
+        }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState(() => ({
+      isLoading: false, movies
+    }))
+  }
+
   componentDidMount() {
     // load movie data
-    setTimeout(this.setLoading, 6000)
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading ? 'Loading...' : 'We are ready'}
-      </div>
+      <section className="container">
+        {isLoading ? (
+        <div className="loader">
+          <span className="loader__text"> Loading...</span>
+        </div>
+        ) : (
+          <div className="movies">
+            {
+              movies.map(({id, year, title, summary, genres, medium_cover_image: poster}) => {
+                return <Movie key={id} year={year} title={title} summary={summary} genres={genres} poster={poster} />
+              })
+            }
+          </div>
+        )}
+      </section>
     );
   }
 }
