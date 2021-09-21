aec6f09d346f792d738d0fff477c177d2670eb03
KimDongKook <pottes11@naver.com>
2021-09-21 20:39:06 +0900

    change project structure(add router, components directory)

---
+++ src/routes/Home.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css'
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  setLoading = () => {
+    this.setState(current => ({
+      isLoading: current.isLoading = !current.isLoading
+    }))
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: {
+          movies
+        }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState(() => ({
+      isLoading: false, movies
+    }))
+  }
+
+  componentDidMount() {
+    // load movie data
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+        <div className="loader">
+          <span className="loader__text"> Loading...</span>
+        </div>
+        ) : (
+          <div className="movies">
+            {
+              movies.map(({id, year, title, summary, genres, medium_cover_image: poster}) => {
+                return <Movie key={id} year={year} title={title} summary={summary} genres={genres} poster={poster} />
+              })
+            }
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
