916a59b9797b788eeb5c5022a731e12479834670
KimDongKook <pottes11@naver.com>
2021-09-21 22:15:43 +0900

    detail component redirect add

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,24 @@
 import React from "react";
 
-function Detail(props) {
-    console.log(props)
-    return <span>hello</span>
+class Detail extends React.Component {
+
+    componentDidMount() {
+        const {location: {state}, history: {push}} = this.props;
+
+        if(state === undefined) {
+            push('/');
+        }
+    }
+    render() {
+        console.log(this.props)
+        const {location: {state}} = this.props;
+        if(state) {
+            const {title} = state;
+            return <span>{title}</span>;
+        } else {
+            return null;
+        }
+    }
 }
 
 export default Detail;
